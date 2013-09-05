d8efdee83771d0cf52bb27b54ce304605d871a3e
rkl-pharmpress <ross.laidlaw@pharmpress.com>
2013-09-05 13:01:21 +0100

    Added simple CXF service and test GET method.

--- src/main/java/com/pharmpress/pepper/services/PepperCxfJaxrsService.java
+++ src/main/java/com/pharmpress/pepper/services/PepperCxfJaxrsService.java
@@ -1,9 +1,7 @@
-/**
- * 
- */
 package com.pharmpress.pepper.services;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 /**
@@ -12,8 +10,8 @@ import javax.ws.rs.Produces;
  */
 public class PepperCxfJaxrsService
 {
-  //
   @GET
+  @Path("test")
   @Produces({"text/plain"})
   public String testGet()
   {
