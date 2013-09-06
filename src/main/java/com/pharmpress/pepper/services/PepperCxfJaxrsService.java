2cd688fbd5c467cd1b74d92b5d327b5bc7c00102
rkl-pharmpress <ross.laidlaw@pharmpress.com>
2013-09-06 11:55:54 +0100

    Added RESTful endpoint for DrugResource and support for XML and JSON outputs.

--- src/main/java/com/pharmpress/pepper/services/PepperCxfJaxrsService.java
+++ src/main/java/com/pharmpress/pepper/services/PepperCxfJaxrsService.java
@@ -4,17 +4,25 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import com.pharmpress.pepper.resources.DrugResource;
+
 /**
+ * A JAX-RS service class that handles HTTP requests.
  * @author Ross Laidlaw
  * @version $Id$
  */
 public class PepperCxfJaxrsService
 {
+  /**
+   * Returns an HTTP response containing a JAX-RS resource that represents a
+   * drug, marshalled to the requested format.
+   * @return an HTTP response containing a JAX-RS resource representing a drug
+   */
   @GET
-  @Path("test")
-  @Produces({"text/plain"})
-  public String testGet()
+  @Path("drug")
+  @Produces({"application/xml", "application/json"})
+  public DrugResource getDrug()
   {
-    return "Test!!!";
+    return new DrugResource("Wonder Drug!");
   }
 }
