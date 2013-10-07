4add4154937c48adafd1f63288c5758eed49dcee
rlaidlaw <rlaidlaw.open@gmail.com>
2013-10-07 10:08:01 +0100

    Added a simple marshalling test using XMLUnit.

--- src/test/java/com/pharmpress/pepper/resources/DrugResourceTest.java
+++ src/test/java/com/pharmpress/pepper/resources/DrugResourceTest.java
@@ -1,10 +1,18 @@
 package com.pharmpress.pepper.resources;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
 /**
  * Implements tests for the {@link DrugResource} class.
@@ -13,15 +21,32 @@ import org.junit.Test;
 public class DrugResourceTest
 {
   /**
-   * @throws JAXBException
+   * Tests that {@link DrugResource drug resources} are marshalled to the
+   * expected XML format.
+   * @throws IOException if {@link Diff} constructor fails
+   * @throws JAXBException if {@link JAXBContext} or {@link Marshaller} fail
+   * @throws SAXException if {@link Diff} constructor fails
    */
   @Test
-  public void testXmlMarshalling() throws JAXBException
+  public void testXmlMarshalling() throws IOException, JAXBException, SAXException
   {
-    DrugResource resource = new DrugResource("Wonder Drug!");
+    String drugName = "paracetamol";
+    String expectedXml =
+      "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"
+      + "<drug>"
+      + "<name>" + drugName + "</name>"
+      + "</drug>";
+    DrugResource resource = new DrugResource(drugName);
     JAXBContext context = JAXBContext.newInstance(resource.getClass());
     Marshaller marshaller = context.createMarshaller();
-    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-    marshaller.marshal(resource, System.out);
+    StringWriter writer = new StringWriter();
+    marshaller.marshal(resource, writer);
+
+    XMLUnit.setIgnoreWhitespace(true);
+    XMLUnit.setIgnoreComments(true);
+    XMLUnit.setIgnoreAttributeOrder(true);
+    Diff diff = new Diff(expectedXml, writer.toString());
+    assertTrue("Differences found between expected XML and output XML: "
+      + diff.toString(), diff.identical());
   }
 }
