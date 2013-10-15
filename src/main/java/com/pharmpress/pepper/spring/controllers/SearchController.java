fc536ae75490219f8bdb7928679aa96aa788e65d
Emma Burrows <emma.burrows@rpharms.com>
2013-10-15 13:41:28 +0100

    Added log4j dependency and properties, and some logging in SearchController.

--- src/main/java/com/pharmpress/pepper/spring/controllers/SearchController.java
+++ src/main/java/com/pharmpress/pepper/spring/controllers/SearchController.java
@@ -3,6 +3,7 @@ package com.pharmpress.pepper.spring.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -24,6 +25,8 @@ import com.pharmpress.pepper.resources.DrugEntity;
 @RequestMapping("/search")
 public class SearchController
 {
+  protected static Logger logger = Logger.getLogger("SearchController");
+
   /**
    * Returns a drug via the search view.
    * @param name the name of the drug (optional)
@@ -57,10 +60,13 @@ public class SearchController
         drugs.add((DrugEntity) object);
       }
       model.addAttribute("drugs", drugs);
+      
+      logger.info("Searched for '" + name + "'");
       session.getTransaction().commit();
     }
     catch (RuntimeException e)
     {
+      logger.error("Error retrieving data using search query '" + name + "': " + e.getMessage());
       session.getTransaction().rollback();
     }
     finally
